56c1512e55562378e47c10d420a9fceb7aae0815
Avery <150257115+wetstickyhands1982@users.noreply.github.com>
2023-12-06 09:06:52 -0500

    added a score label, made the apple respawn when it gets eaten, gave apple random position when it spawns and made elephant able to eat apple

--- Elephant.java
+++ Elephant.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Elephant here.
+ * The Elephant, my GLORIOUS king
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (AVERY LI) 
+ * @version (nov 23 2023)
  */
 public class Elephant extends Actor
 {
@@ -14,6 +14,6 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        move(1);
     }
 }
